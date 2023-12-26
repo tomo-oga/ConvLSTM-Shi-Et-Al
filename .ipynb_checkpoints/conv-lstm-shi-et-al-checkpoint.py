944a166c320cb10f2c1d542ddcfdc16eaa9e7143
Tomohiro Oga <oga.t@northeastern.edu>
2023-12-25 21:07:32 -0500

    Corrected for right weight initialization and dimensionality

--- .ipynb_checkpoints/conv-lstm-shi-et-al-checkpoint.py
+++ .ipynb_checkpoints/conv-lstm-shi-et-al-checkpoint.py
@@ -15,30 +15,30 @@ class ConvLSTMCell(nn.Module):
         
         # input gate weights
         self.W_xi = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
-        self.W_hi = nn.Conv2d(hidden_dims, hidden_dims, kernel_size, padding=padding, bias=False)
+        self.W_hi = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
         self.W_ci = nn.Parameter(torch.Tensor(hidden_dims, h, w).uniform_(-dist, dist))
         
         self.b_i = nn.Parameter(torch.zeros(hidden_dims)) #bias term
         
         # forget gate weights
         self.W_xf = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
-        self.W_hf = nn.Conv2d(hidden_dims, hidden_dims, kernel_size, padding=padding, bias=False)
+        self.W_hf = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
         self.W_cf = nn.Parameter(torch.Tensor(hidden_dims, h, w).uniform_(-dist, dist))
         
         self.b_f = nn.Parameter(torch.zeros(hidden_dims)) #bias term
         
         # cell state memory gate weights
         self.W_xc = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
-        self.W_hc = nn.Conv2d(hidden_dims, hidden_dims, kernel_size, padding=padding, bias=False)
+        self.W_hc = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
         
         self.b_c = nn.Parameter(torch.zeros(hidden_dims)) #bias term
         
         # output gate weights
         self.W_xo = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
-        self.W_ho = nn.Conv2d(hidden_dims, hidden_dims, kernel_size, padding=padding, bias=False)
-        self.W_co = nn.Parameter(torch.Tensor(hidden_dims, h, w).uniform_(-dist, dist))
+        self.W_ho = nn.Conv2d(in_dims, hidden_dims, kernel_size, padding=padding, bias=False)
+        self.W_co = nn.Parameter(torch.empty(hidden_dims, h, w))
         
-        self.b_o = nn.Parameter(torch.zeros(hidden_dims)) #bias term
+        self.b_o = nn.Parameter(torch.Tensor(hidden_dims, h, w).uniform_(-dist, dist))
         
                     
     def forward(self, x_t, prev):
@@ -46,7 +46,7 @@ class ConvLSTMCell(nn.Module):
         h_t, c_t = prev
         
         # not shown well but h_t and c_t are of previous time steps, x_t is of current
-        
+              
         i_t = F.sigmoid(self.W_xi(x_t) + self.W_hi(h_t) + self.W_ci * c_t + self.b_i.view(1,self.hidden_dims,1,1))
         f_t = F.sigmoid(self.W_xf(x_t) + self.W_hf(h_t) + self.W_cf * c_t + self.b_f.view(1,self.hidden_dims,1,1))
         c_t = f_t * c_t + i_t * F.tanh(self.W_xc(x_t) + self.W_hc(h_t) + self.b_c.view(1,self.hidden_dims,1,1))
